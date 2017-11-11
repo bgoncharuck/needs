51b740e73e07e5678c950bc1e0b04e76d07ebd8b
bgoncharuck <dariydragon26@gmail.com>
2017-11-11 23:14:42 +0200

    scriptCommit

--- sorting.h
+++ sorting.h
@@ -1,38 +1,32 @@
-void insertionSort(int array[], long size) {
-	long externalCount = 0;
-	long internalCount = 0;
-	long temp = 0;
+void insertionSort(int arr[], long size) {
 	if(size < 100000)
-		for(externalCount = size; externalCount > 0; externalCount--) {
-			internalCount = externalCount;
-			while(internalCount < size && array[internalCount] < array[internalCount - 1]) {
-				swapInt(&array[internalCount], &array[internalCount - 1]);
-				internalCount += 1;
-			}
-		}
+		for(long i = 1; i < size; i++)
+		 	for(long j = i; j > 0 && arr[j] < arr[j-1]; j--)
+				swapInt(&arr[j], &arr[j-1]);
 	else if(size >= 100000)
-		for(externalCount = 1; externalCount < size; externalCount++) {
-			temp = array[externalCount];
-			internalCount = externalCount - 1;
-			while(internalCount >= 0  && array[internalCount] > temp) {
-				array[internalCount + 1] = array[internalCount];
-				internalCount -= 1;
+		for(long i = 1; i < size; i++) {
+			long key = arr[i];
+			long j = i - 1;
+			while(j >= 0  && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j -= 1;
 			}
-			array[internalCount+1] = temp;
+			arr[j+1] = key;
 		}
 	return;
 }
 
-void selectionSort(int array[], int size) {
-	int compareIndex = 0;
-	int externalCount = 0;
-	int internalCount = 0;
-	for(externalCount = 0; externalCount < size; externalCount++) {
-		compareIndex = externalCount;
-		for(internalCount = externalCount + 1; internalCount < size; internalCount++)
-		if(array[internalCount] < array[compareIndex])
-		compareIndex = internalCount;
-		swapInt(&array[externalCount], &array[compareIndex]);
+void selectionSort(int arr[], long size) {
+	long tmp = (size > 100000) ? 1 : 0;
+	for (long i = 0; i < size - tmp; i++) {
+		long minIndex = i;
+		for (long j = i + 1; j < size; j++) {
+			if (arr[j] < arr[minIndex]) {
+				minIndex = j;
+			}
+		}
+		if (i != minIndex) {
+			swapInt(&arr[i], &arr[minIndex]);
+		}
 	}
-	return;
 }
